bff0bfbb58c0840a6a300c9664975f0500c192dd
Pascal Sancho <psancho.13@gmail.com>
2025-05-29 15:08:10 +0200

    MAINT cleanup suite à déplacement dans galeizon du code

--- src/Model/ConfException.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace Psancho\Comic\Model;
-
-use RuntimeException;
-
-class ConfException extends RuntimeException {}
--- src/Model/Database/ConnectionException.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types = 1);
-
-namespace Psancho\Comic\Model\Database;
-
-use RuntimeException;
-
-class ConnectionException extends RuntimeException
-{}
